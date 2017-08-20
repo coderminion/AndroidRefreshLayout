42a62c7a8a045cb22e7d547a67e8edb66e9f63b5
suhas <vaibhv.kadam45@gmail.com>
2017-08-20 16:21:56 +0530

    Added Screenshots

---
+++ app/src/main/java/com/coderminion/refreshlayout/Constants.java
@@ -0,0 +1,16 @@
+package com.coderminion.refreshlayout;
+
+/**
+ * Created by vaibhav on 20/8/17 .
+ */
+
+
+public class Constants {
+
+    //TODO change this url as per your needs
+    public static final String REMOTE_URL = "http://api.coderminion.com";
+
+    //It will get appended to our REMOTE_URL then our BASE_URL in Webservice.java be like : http://api.coderminion.com/androidversions.json
+    public static final String ANDROIDVERSIONS = "/androidversions.json";
+
+}
